1ae52d6aa1b211e6802fae47ac46ea4d1e5553b1
Rando Christensen <digiwano@gmail.com>
2015-06-03 12:51:46 -0700

    directly specify aws region

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -2,7 +2,7 @@
 source /var/scripts/env.prop
 
 (cd $WWW_ROOT && \
-  aws s3 cp s3://configs-and-scripts/${APP_NAME}/configs.${NODE_ENV}.json ./configs.json && \
+  aws s3 cp --region ${AWS_REGION} s3://configs-and-scripts/${APP_NAME}/configs.${NODE_ENV}.json ./configs.json && \
   mkdir node_modules && \
   npm install --production && \
   exit 0) || exit 1
