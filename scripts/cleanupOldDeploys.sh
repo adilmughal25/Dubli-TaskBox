e0f09c6b40e7ccae488ad0b4d845dc4a905c678c
Rando Christensen <digiwano@gmail.com>
2015-06-18 11:30:30 -0700

    whoops, clean this up a bit

--- scripts/cleanupOldDeploys.sh
+++ scripts/cleanupOldDeploys.sh
@@ -11,14 +11,23 @@ function get_real_dir {
 
 DEPLOYMENT_GROUP_ID=$(aws deploy get-deployment-group --region ${AWS_REGION} --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --output text | grep DEPLOYMENTGROUPINFO | awk '{print $4}')
 DEP_ROOT=$(get_real_dir "/opt/codedeploy-agent/deployment-root")
+if [ ! -d "$DEP_ROOT" ]; then
+  echo "Can't find required dir DEP_ROOT:$DEP_ROOT"
+  exit
+fi
 CURRENT_GROUP_ROOT=$(get_real_dir "${DEP_ROOT}/${DEPLOYMENT_GROUP_ID}")
+if [ ! -d "$CURRENT_GROUP_ROOT" ]; then
+  echo "Can't find required dir CURRENT_GROUP_ROOT:$CURRENT_GROUP_ROOT"
+  exit
+fi
 CURRENT_DEPLOYMENT_DIR=$(get_real_dir "${CURRENT_GROUP_ROOT}/${DEPLOYMENT_ID}")
+if [ ! -d "$CURRENT_DEPLOYMENT_DIR" ]; then
+  echo "Can't find required dir CURRENT_DEPLOYMENT_DIR:$CURRENT_DEPLOYMENT_DIR"
+  exit
+fi
 PREVIOUS_DEPLOYMENT_DIR=$(get_real_dir $(cat ${DEP_ROOT}/deployment-instructions/${DEPLOYMENT_GROUP_ID}_last_successful_install 2>/dev/null))
 DEPLOYMENTS_LIST=$(find ${CURRENT_GROUP_ROOT} -type d -mindepth 1 -maxdepth 1)
 
-if [ ! -d "$DEP_ROOT" ]; exit; fi
-if [ ! -d "$CURRENT_GROUP_ROOT" ]; exit; fi
-if [ ! -d "$CURRENT_DEPLOYMENT_DIR" ]; exit; fi
 
 for DIR in $DEPLOYMENTS_LIST; do
   REAL_PATH=$(get_real_dir $DIR)
