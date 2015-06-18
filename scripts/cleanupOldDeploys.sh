752ee01b05fccc776d8c67d5b8e25bb3a4e9b634
Rando Christensen <digiwano@gmail.com>
2015-06-18 11:24:40 -0700

    test version of cleanup script

---
+++ scripts/cleanupOldDeploys.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -x
+PATH=$PATH:/usr/local/bin
+source /var/scripts/env.prop
+
+function get_real_dir {
+  D=$1
+  if [ -z "$D" ]; then return; fi
+  if [ ! -d "$D" ]; then return; fi
+  echo $( cd $1 && pwd )
+}
+
+DEPLOYMENT_GROUP_ID=$(aws deploy get-deployment-group --region ${AWS_REGION} --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --output text | grep DEPLOYMENTGROUPINFO | awk '{print $4}')
+DEP_ROOT=$(get_real_dir "/opt/codedeploy-agent/deployment-root")
+CURRENT_GROUP_ROOT=$(get_real_dir "${DEP_ROOT}/${DEPLOYMENT_GROUP_ID}")
+CURRENT_DEPLOYMENT_DIR=$(get_real_dir "${CURRENT_GROUP_ROOT}/${DEPLOYMENT_ID}")
+PREVIOUS_DEPLOYMENT_DIR=$(get_real_dir $(cat ${DEP_ROOT}/deployment-instructions/${DEPLOYMENT_GROUP_ID}_last_successful_install 2>/dev/null))
+DEPLOYMENTS_LIST=$(find ${CURRENT_GROUP_ROOT} -type d -mindepth 1 -maxdepth 1)
+
+if [ ! -d "$DEP_ROOT" ]; exit; fi
+if [ ! -d "$CURRENT_GROUP_ROOT" ]; exit; fi
+if [ ! -d "$CURRENT_DEPLOYMENT_DIR" ]; exit; fi
+
+for DIR in $DEPLOYMENTS_LIST; do
+  REAL_PATH=$(get_real_dir $DIR)
+  if [ \
+    ! -z "${REAL_PATH}" -a \
+    "${REAL_PATH}" != "${CURRENT_DEPLOYMENT_DIR}" -a \
+    "${REAL_PATH}" != "${PREVIOUS_DEPLOYMENT_DIR}" -a \
+    "${REAL_PATH:0:${#CURRENT_GROUP_ROOT}}" == $CURRENT_GROUP_ROOT
+  ]; then
+    echo "TEST: RM-DASH-R-F ${REAL_PATH}"
+  fi
+done
