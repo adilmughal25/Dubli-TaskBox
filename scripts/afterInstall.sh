23eb1a738e5bf0fc4c973623b02e852e9d7d5e90
Jordan Linxwiler <jlinxwiler11@gmail.com>
2015-07-29 09:32:21 -0700

    Changing the json being used for stage config to the consolidated one in configs-and-scripts

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -4,7 +4,7 @@ PATH=$PATH:/usr/local/bin
 source /var/scripts/env.prop
 
 (cd $WWW_ROOT && \
-  aws s3 cp --region ${AWS_REGION} s3://configs-and-scripts/${APP_NAME}/configs.${NODE_ENV}.json ./configs.json && \
+  aws s3 cp --region ${AWS_REGION} s3://configs-and-scripts/configs.${NODE_ENV}.json ./configs.json && \
   mkdir node_modules && \
   npm install --production && \
   exit 0) || exit 1
