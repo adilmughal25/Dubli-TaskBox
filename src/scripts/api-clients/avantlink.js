58ec919cd72e0ffe230ae728bb62e13c507fcca8
jrosemeier79 <jrosemeier79@gmail.com>
2015-08-24 13:49:48 -0400

    save work in progress on avantLink - resetting dev box

--- src/scripts/api-clients/avantlink.js
+++ src/scripts/api-clients/avantlink.js
@@ -1,52 +1,97 @@
 "use strict";
 
-const AUTH_KEY = '401dc5ad219d787ee5ff88d38872c8d5';
-const BASE_URL = 'https://classic.avantlink.com/api.php';
-const AFFILIATE_ID = '147618';
-const WEBSITE_ID = '183130';
+const _ = require('lodash');
+const debug = require('debug')('avantlink:api-client');
+const denodeify = require('denodeify');
+const querystring = require('querystring');
+const request = require('request-promise');
+const jsonify = require('./jsonify-xml-body');
+const limiter = require('ominto-utils').promiseRateLimiter;
+// debugging the requests || TODO: remove after finishing implementation
+//require('request-promise').debug = true; 
 
-var _ = require('lodash');
-var denodeify = require('denodeify');
-var querystring = require('querystring');
-var request = require('request-promise');
-var jsonify = require('./jsonify-xml-body');
+const API_CREDENTIALS = {
+  us: {
+    baseUrl: 'https://classic.avantlink.com/api.php',
+    authKey: '401dc5ad219d787ee5ff88d38872c8d5',
+    affiliateId: 147618,
+    websiteId: 183130
+  },
+  ca: {
+    baseUrl: 'https://classic.avantlink.ca/api.php',
+    authKey: '486155f4cb679938e970330b6a620df8',
+    affiliateId: 150082,
+    websiteId: 186354
+  }
+};
 
-function createClient() {
+const API_TYPES = {
+  merchants: {
+    module: 'AssociationFeed',
+    limit: [500, 84600], // 500 requests per day max
+    defParams: {
+      output: 'json',
+      association_status: 'active'
+    }
+  },
+  promos: {
+    module: 'AdSearch',
+    limit: [6000, 3600], // 7200 request(s) per hour. 150000 request(s) per day.
+    defParams: {
+      output: 'xml',
+      search_results_sort_order: 'Merchant Id',
+      show_contextual_analysis: 0,
+      show_ad_relevance: 0,
+      ad_type: '',  //  "dotd-html" (Deal of the Day-DynHTML), "dotd-text" (DotD-DynText), flash, html, image, text, and video. Default is to return all.
+    }
+  }
+};
+
+function avantLinkClient(s_region) {
+  if (!s_region) s_region = 'us';
+  if (!API_CREDENTIALS[s_region]) throw new Error("Unknown AvantLink region: "+s_region);
+
+  var creds = API_CREDENTIALS[s_region];
+
+  // default request options
   var client = request.defaults({
-    resolveWithFullResponse: true
+    uri: creds.baseUrl,
+    json: true,
+    simple: true,
+    resolveWithFullResponse: false,
+    qs: {
+      affiliate_id: creds.affiliateId,
+      auth_key: creds.authKey,
+      website_id: creds.websiteId
+    }
   });
 
   client.getMerchants = function() {
-    var url = BASE_URL + '?' + querystring.stringify({
-      module: 'AssociationFeed',
-      auth_key: AUTH_KEY,
-      affiliate_id: AFFILIATE_ID,
-      output: 'json',
-      association_status: 'active'
-    });
+    let arg = {
+      json: (API_TYPES.merchants.defParams.output == 'json' ? true : false),
+      qs: _.merge({}, API_TYPES.merchants.defParams, {
+        module: API_TYPES.merchants.module,
+    })};
 
-    return client.get({
-      uri: url,
-      json: true
-    }).then(resp => resp.body);
+    return client.get(arg);
   };
 
-  client.getTextLinks = function() {
-    var url = BASE_URL + '?' + querystring.stringify({
-      module: 'AdSearch',
-      affiliate_id: AFFILIATE_ID,
-      website_id: WEBSITE_ID,
-      output: 'xml',
-      ad_type: 'text'
-    });
+  client.getPromotions = function() {
+    let arg = {
+      json: (API_TYPES.promos.defParams.output == 'json' ? true : false),
+      qs: _.merge({}, API_TYPES.promos.defParams, {
+        module: API_TYPES.promos.module
+    })};
 
-    var promise = client.get(url)
+    let result = client.get(arg)
       .then(jsonify)
-      .then(data => data.NewDataSet.Table1);
-    return promise;
+      .then(data => data.NewDataSet.Table1)
+    ;
+
+    return result;
   };
 
   return client;
 }
 
-module.exports = createClient;
+module.exports = avantLinkClient;
