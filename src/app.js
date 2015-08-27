6259539ff1950bbf3cf77e4f1eca51106779b9c7
Rando Christensen <digiwano@gmail.com>
2015-08-27 15:26:28 -0700

    further taskbox refactor: move scheduler into its own file

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 "use strict";
 
 const configs = require('../configs.json');
-const uuid = require('node-uuid');
 const bunyan = require('bunyan');
-const co = require('co');
-const schedule = require('node-schedule');
-const debug = require('debug')('taskbox:tasks');
-const _ = require('lodash');
-const prettyMs = require('pretty-ms');
 
-const affiliateTasks = require('./affiliate-tasks'); // perhaps put this in ./tasks/affiliate-networks/index.js later?
+const scheduleTasks = require('./schedule-tasks');
+const affiliates = require('./affiliate-tasks'); // perhaps put this in ./tasks/affiliate-networks/index.js later?
 const ftpToS3 = require('./ftp-to-s3');
 const snsPing = require('./tasks/sns-ping');
 
@@ -33,52 +28,12 @@ function init(id) {
     serializers: bunyan.stdSerializers
   });
   log.level(configs.logLevel);
-  var isDev = /^dev/.test(process.env.NODE_ENV);
-  var runOnStartOnly = !!process.env.RUN_ON_START_ONLY;
-  var runOnStart = runOnStartOnly || !!process.env.RUN_ON_START;
 
-  var schedules = {};
+  const createTask = scheduleTasks(log);
 
   ftpToS3(log, configs.ftpToS3);
-  affiliateTasks.init(createTask);
+  affiliates.init(createTask);
   createTask('TownClock SNS Ping', snsPing.ping, {minute: [0,15,30,45]});
-
-  function taskRunner(name, task) {
-    return function() {
-      var start = Date.now();
-      log.info(name +" started");
-      co(task).then(function() {
-        var end = Date.now();
-        var elapsed = prettyMs(end-start, {verbose:true});
-        log.info(name + " successfully completed in "+ elapsed);
-      }).catch(function(error) {
-        if (error === "already-running") {
-          debug(name + " is currently already running. Waiting until next run-time");
-          return;
-        }
-        log.error(name + " ERROR OCCURED: " + ('stack' in error) ? error.stack : error);
-      });
-    };
-  }
-
-  function createTask(name, task, spec) {
-    // using task_t so i can pretend i'm a C programmer for a line of code
-    var task_t = typeof task, isTask = task_t === 'function';
-    if (!isTask) throw new Error("can't create task for "+name+": passed task is not a function! (is:"+task_t+")");
-    var id = name.replace(/(?:\W+|^)(\w)/g, (m,letter) => letter.toUpperCase());
-    var rule = new schedule.RecurrenceRule();
-    _.extend(rule, spec);
-
-    if (!isDev || !runOnStartOnly) {
-      // log.info("Schedule task: "+name+" ("+id+") with spec: "+JSON.stringify(spec)+"");
-      schedules[id] = schedule.scheduleJob(spec, taskRunner(name, task));
-    }
-
-    if (isDev && runOnStart) {
-      debug("Dev Mode: Starting task `%s` immediately", id);
-      taskRunner('(dev-autostart) '+name, task)();
-    }
-  }
 }
 
 module.exports = {
