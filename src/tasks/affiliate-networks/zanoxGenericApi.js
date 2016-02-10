b65ed68313072c5d7c6f8a62aa4a30740e59cde8
Rando Christensen <digiwano@gmail.com>
2016-02-10 14:04:05 -0800

    fix for zanox debugging info

--- src/tasks/affiliate-networks/zanoxGenericApi.js
+++ src/tasks/affiliate-networks/zanoxGenericApi.js
@@ -2,7 +2,7 @@
 
 const _ = require('lodash');
 const co = require('co');
-const debug = require('debug')('zanox:processor');
+const _debug = (a,b) => require('debug')(['zanox', 'processor', a, b].join(':'));
 const moment = require('moment');
 const utils = require('ominto-utils');
 const sendEvents = require('./support/send-events');
@@ -24,6 +24,7 @@ const STATE_MAP = {
 };
 
 const ZanoxGenericApi = function(s_region, s_entity) {
+  const debug = _debug(s_region, s_entity);
   if (!(this instanceof ZanoxGenericApi)) {
     debug("instantiating ZanoxGenericApi for: %s-%s", s_entity, s_region);
     return new ZanoxGenericApi(s_region, s_entity);
