75804ff2adc11319173c16dc736005fd3401d462
snandepu <snandepu@gmail.com>
2016-05-16 14:03:25 -0500

    Revert "Disable Commission Importer, Until Research is done and fix it."

--- src/tasks/affiliate-networks/index.js
+++ src/tasks/affiliate-networks/index.js
@@ -166,8 +166,8 @@ const zanoxDubliGlobalApi = zanoxGenericApi('global', 'dubli');
 
 function init(tasker) {
   initializeMerchantImporters(tasker);
-  //initializeCommissionsProcessors(tasker);
-  //initializeCommissionsDubliProcessors(tasker);
+  initializeCommissionsProcessors(tasker);
+  initializeCommissionsDubliProcessors(tasker);
 }
 
 
