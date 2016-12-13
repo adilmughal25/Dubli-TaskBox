9d913e53955aa2aa782c8b2987f616ce170ad671
dhruva kumar <dkumar@ominto.com>
2016-12-12 21:45:11 -0600

    #refer-friend

---
+++ src/tasks/affiliate-networks/user-referrals/tasks.js
@@ -0,0 +1,33 @@
+/**
+ * Created by dhruva on 9/20/16.
+ */
+"use strict";
+
+const configs = require('../../../../configs.json');
+const utils = require('ominto-utils');
+utils.clients.init(configs);
+const singleRun = require('../support/single-run');
+const dataClient = utils.restClient(configs.data_api);
+
+const request = require('request-promise');
+
+const addReferralPropertiesApi = function() {
+
+    if (!(this instanceof addReferralPropertiesApi)) {
+        return new addReferralPropertiesApi();
+    }
+    
+    this.addReferralProperties = singleRun(function*() {
+        
+        let newInvitedUsers= yield dataClient.get('/getNewInvitedFriends',false,this);
+        let getNewInvitedSocialFriends= yield dataClient.get('/getNewInvitedSocialFriends',false,this);
+        let getOtherFriends= yield dataClient.get('/getOtherFriends',false,this);
+    });
+
+    this.addReferralAmount=singleRun(function* () {
+        let addReferralAmountForAvailableBalance = yield dataClient.get('/addReferralAmountForAvailableBalance',false,this);
+        let VipReferralAmount = yield dataClient.get('/VipReferralAmount',false,this);
+    })
+}
+
+module.exports = addReferralPropertiesApi;
