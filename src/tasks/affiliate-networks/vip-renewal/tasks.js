ef3c12dfb821987a81b48f6def6f93ebdb069250
Duc Do <ddo@ominto.com>
2017-01-18 14:19:31 -0600

    (OM-1746) VIP email renewal

---
+++ src/tasks/affiliate-networks/vip-renewal/tasks.js
@@ -0,0 +1,36 @@
+/**
+ * Created by dhruva on 9/20/16.
+ */
+"use strict";
+
+const configs = require('../../../../configs.json');
+const utils = require('ominto-utils');
+utils.clients.init(configs);
+const singleRun = require('../support/single-run');
+const directKinesisPut = require('../support/direct-kinesis-put');
+const dataClient = utils.restClient(configs.data_api);
+
+const sendVIPRenewalReminderApi = function() {
+
+    if (!(this instanceof sendVIPRenewalReminderApi)) {
+        return new sendVIPRenewalReminderApi();
+    }
+
+    this.sendEmailReminder = singleRun(function*() {
+      // Send email reminder for VIP Customer when VIP expired in 1, 7 and 30 days before
+      let dateToExpiration = 30;
+      let result = yield dataClient.get('/getAllUsersByVIPExpirationDate/' + dateToExpiration, {}, this);
+      let userList = result.body;
+
+
+      // trigger event to send email to each user in the list
+      // FOR TESTING PURPOSE ONLY: sending email to ddo@ominto.com, userId 4503995
+      yield directKinesisPut('user-consolidated', 'userDid:vipRenewalReminder', {
+          user: {
+            id: 4503995
+          }
+      }, null, "vip-renewal-reminder");
+    });
+}
+
+module.exports = sendVIPRenewalReminderApi;
