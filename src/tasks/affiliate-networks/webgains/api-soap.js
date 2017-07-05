20e155dfab6bcbb3853629eb7f3d18994a69335c
prash <pkagwad@ominto.com>
2017-07-05 12:13:07 -0500

    Merge pull request #327 from pkagwad/dev
    OM-2194 - Update Affiliate Credentials in Taskbox [Webgains]

--- src/tasks/affiliate-networks/webgains/api-soap.js
+++ src/tasks/affiliate-networks/webgains/api-soap.js
@@ -23,7 +23,7 @@ const API_SERVICE_WSDL = 'http://ws.webgains.com/aws.php?wsdl';
 const API_CFG = {
   ominto: {
     user: 'merchants@ominto.com',
-    pass: 'DubLi2017',
+    pass: 'Dubot2017#',
     siteId: 177143
   },
 };
