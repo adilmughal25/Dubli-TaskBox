47c106ed07bdb59c2933e9383a9306c0d763b27f
cmrameshkumar <32033626+cmrameshkumar@users.noreply.github.com>
2017-11-05 15:45:09 +0400

    Update applicationStart.sh
    Start script changed to start with PM2

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,9 +1,10 @@
 # service awslogs start
-if ( status awslogs-upstart | grep stop ); then
-  echo "Current User:"$USER
-  start awslogs-upstart
-fi
-if ( status nodeApp | grep stop ); then
-  start nodeApp
-fi
+#if ( status awslogs-upstart | grep stop ); then
+#  echo "Current User:"$USER
+#  start awslogs-upstart
+#fi
+#if ( status nodeApp | grep stop ); then
+#  start nodeApp
+#fi
+pm2 start /var/www/src/server.js --node-args="--harmony"
 exit 0
