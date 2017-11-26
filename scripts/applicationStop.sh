dd633cded6a79767e3f195c497ff0c43fd82d2b9
Ali Raza Alvi <aliraza432@gmail.com>
2017-11-26 14:16:34 +0400

    Update PM2 stop to stop the application

--- scripts/applicationStop.sh
+++ scripts/applicationStop.sh
@@ -1,12 +1,3 @@
-## this is the 'default' one, but we use one that has
-#if ( service awslogs status | grep running ); then
-#  service awslogs stop
-#fi
-#if ( status awslogs-upstart | grep running ); then
-#  stop awslogs-upstart
-#fi
-#if ( status nodeApp | grep start ); then
-#  stop nodeApp
-#fi
+#!/bin/bash
 pm2 stop all
 exit 0
