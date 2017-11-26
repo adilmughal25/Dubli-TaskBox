ec7e04b17a6014876f5ec035be0ea20432705009
Ali Raza Alvi <aliraza432@gmail.com>
2017-11-26 14:15:38 +0400

    Update on application start script for PM2

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,10 +1,4 @@
 # service awslogs start
-#if ( status awslogs-upstart | grep stop ); then
-#  echo "Current User:"$USER
-#  start awslogs-upstart
-#fi
-#if ( status nodeApp | grep stop ); then
-#  start nodeApp
-#fi
-pm2 start /var/www/src/server.js --node-args="--harmony"
+cd /var/www
+pm2 start src/server.js --node-args="--harmony"
 exit 0
