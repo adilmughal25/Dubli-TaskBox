c10885176e523472a34ea1a8407fb49a4596e2ee
Ali Raza Alvi <aliraza432@gmail.com>
2017-12-20 16:31:13 +0400

    Setting node environment variable from ec2 tags.

--- scripts/applicationStart.sh
+++ scripts/applicationStart.sh
@@ -1,4 +1,7 @@
 # service awslogs start
+fetch_tags_cmd="aws --region ${AWS_REGION} autoscaling describe-auto-scaling-groups --auto-scaling-group-names ${AWS_AUTOSCALE_GROUP} --query AutoScalingGroups[0].Tags --output text"
+NODE_ENV=$(${fetch_tags_cmd} | grep "^env\s" | cut -f5)
+env NODE_ENV="${NODE_ENV}"
 cd /var/www
 pm2 start src/server.js --node-args="--harmony"
 exit 0
