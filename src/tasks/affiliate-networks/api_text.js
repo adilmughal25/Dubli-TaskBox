9a58307f5827dbca64bec6e4c00fc8e240423eb5
msadil <adil.ymughal@gmail.com>
2023-11-02 14:14:04 +0500

    last commit

--- src/tasks/affiliate-networks/api_text.js
+++ src/tasks/affiliate-networks/api_text.js
@@ -1,3 +1,34 @@
+// /*const axios = require('axios');
+
+// const API_ACCOUNT_NUMBER = 'a16120154459';
+// const API_BASE_URL = 'https://api.a8.net/as/'+API_ACCOUNT_NUMBER+'/pointreport/';
+// const API_USERNAME = 'merchants';
+// const API_PASSWORD = 'Ominto2016';
+
+// /*const res = axios({
+//     method: 'get',
+    
+//     params:{
+//         user : API_USERNAME,
+//       pass : API_PASSWORD
+
+//     }
+// }); 
+// console.log(res);*/
+// axios.get({
+//   url:API_BASE_URL,
+//   params:{
+//     user : API_USERNAME,
+//       pass : API_PASSWORD
+//   }
+// })
+//   .then(response => {
+//     console.log(response.data);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
 const axios = require('axios');
 
 const API_ACCOUNT_NUMBER = 'a16120154459';
@@ -5,13 +36,18 @@ const API_BASE_URL = 'https://api.a8.net/as/'+API_ACCOUNT_NUMBER+'/pointreport/'
 const API_USERNAME = 'merchants';
 const API_PASSWORD = 'Ominto2016';
 
-const res = axios({
-    method: 'get',
-    url:API_BASE_URL,
-    params:{
-        user : API_USERNAME,
-      pass : API_PASSWORD
-
-    }
-}); 
-console.log(res);
+axios({
+  method: 'get',
+  url: API_BASE_URL,
+  params: {
+    user: API_USERNAME,
+    pass: API_PASSWORD
+  }
+})
+  .then(function (response) {
+    console.log('awais');
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
